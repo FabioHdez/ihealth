a961f223bf84102e71ed084dbfe96f25b73e07e7
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2022-03-10 16:00:34 -0500

    homepage template and some changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const exphbs = require('express-handlebars')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+// Template engine
+app.engine('hbs', exphbs.engine({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs');
+app.use(express.static('./public'))
+
+
+var home = require('./routes/home')
+app.use('/',home);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started")
