1ae046fc564391a264baf0728477a8d77db6e142
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2022-06-06 13:28:03 -0400

    Employee/User db model

---
+++ models/Employee.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bcrypt = require('bcryptjs');
+
+const EmployeeSchema = new Schema({
+  name: {type: String, required: true},
+  phone: {type: String, required: false},
+  email: {type:String,required:false},
+  password: { type: String, required: false },
+  deleted: {type: Boolean,default: false,required: true}
+})
+EmployeeSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+EmployeeSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
